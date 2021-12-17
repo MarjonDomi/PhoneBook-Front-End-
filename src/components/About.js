4e503d69929b47b5c548ba2e402e2741b0c9caaa
MarjonDomi <49747111+MarjonDomi@users.noreply.github.com>
2021-12-17 15:22:30 +0100

    new commit

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const About = () => {
     return (
         <div className="text-center">
-       <h4>Version 1.0.0</h4>
+       <h4>Version 0.1.0</h4>
         </div>
     )
 }
